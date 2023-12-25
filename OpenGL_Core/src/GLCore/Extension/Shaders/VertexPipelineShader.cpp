c9ad1bfcaf6e79f816370172afa63f1b46760ed5
Devin Park <devinpark345@gmail.com>
2023-12-25 11:04:37 -0500

    Adding Geometry Shader to GLCore::Extension::Shaders::VertexPipelineShader

--- OpenGL_Core/src/GLCore/Extension/Shaders/VertexPipelineShader.cpp
+++ OpenGL_Core/src/GLCore/Extension/Shaders/VertexPipelineShader.cpp
@@ -10,4 +10,14 @@ namespace GLCore::Extension::Shaders {
 		addPrimitiveShader(new PrimitiveShader(GL_VERTEX_SHADER, vertPath));
 		addPrimitiveShader(new PrimitiveShader(GL_FRAGMENT_SHADER, fragPath));
 	}
+
+	VertexPipelineShader::VertexPipelineShader(
+		const std::string& name, 
+		const std::string& vertPath,
+		const std::string& geomPath, 
+		const std::string& fragPath) : Shader(name) {
+		addPrimitiveShader(new PrimitiveShader(GL_VERTEX_SHADER, vertPath));
+		addPrimitiveShader(new PrimitiveShader(GL_GEOMETRY_SHADER, geomPath));
+		addPrimitiveShader(new PrimitiveShader(GL_FRAGMENT_SHADER, fragPath));
+	}
 }
--- OpenGL_Core/src/GLCore/Extension/Shaders/VertexPipelineShader.hpp
+++ OpenGL_Core/src/GLCore/Extension/Shaders/VertexPipelineShader.hpp
@@ -7,5 +7,6 @@ namespace GLCore::Extension::Shaders {
 	public:
 		VertexPipelineShader() = default;
 		VertexPipelineShader(const std::string& name, const std::string& vertPath, const std::string& fragPath);
+		VertexPipelineShader(const std::string& name, const std::string& vertPath, const std::string& geomPath, const std::string& fragPath);
 	};
 }
